ec17e1645f8be9eeb0bdaffea7a6771a5fa895f1
younghiro <younghiromu112010@icloud.com>
2023-01-03 18:51:13 +0100

    add two files

---
+++ CompanyEntranceSystem/Human.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace CompanyEntranceSystem
+{
+    public abstract class Human
+    {
+        public string Name { get; set; }
+        public int PassWord { get; set; }
+        public int CompanyNumber { get; set; }
+        public bool State { get; set; }　//true means the person is already inside
+
+        protected Human(string name, int password, int companyNumber, bool state)
+        {
+            Name = name;
+            PassWord = password;
+            CompanyNumber = companyNumber;
+            State = state;
+        }
+    }
+
+    public class Worker : Human
+    {
+        public string Department { get; set; }
+
+        private Worker(string name, int password, string department,int companyNumber, bool state) : base(name, password, companyNumber,state)
+        {
+            Department = department;
+        }
+    }
+
+    public class Visitor : Human
+    {
+        public string Explanation { get; set; }
+
+        private Visitor(string name, int password, string explanation, int companyNumber, bool state) :  base(name, password, companyNumber, state)
+        {
+            Explanation = explanation;
+            password = 000;
+        }
+    }
+    
+}
+
---
+++ CompanyEntranceSystem/db.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using CompanyEntranceSystem;
+
+namespace CompanyEntranceSystem
+{
+    public class db
+    {
+        List<Worker> workers = new List<Worker>(); //Worker(string name, int password, int company_number, string department, int companyNumber, bool state) 
+        List<Visitor> visitors = new List<Visitor>(); //Visitor(string name, int password, string explanation, int companyNumber, bool state) 
+        //Customers.Add(new Customer("John", "Doe", "Prague", 1985));
+
+        //List<List<string>> dataList = new List<List<string>>();
+
+        workers.Add(new List<Worker>( "Alex", 555, "company president", 001,true));
+        workers.Add(new List<Worker>( "bob", 555, "sw engineer", 002,false));
+        workers.Add(new List<Worker>( "tomas", 555, "sales", 003,true));
+        workers.Add(new List<Worker>( "Luis", 555, "sales",004, true));
+            
+        visitors.Add(new List<Visitor>("watoson","He is the son of Alex",000, true));
+        visitors.Add(new List<Visitor>("Lisa","she is the wife of bob",000, false));
+
+    }
+}
+
